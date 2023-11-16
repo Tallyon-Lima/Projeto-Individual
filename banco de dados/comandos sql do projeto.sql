a00e5b392e90c358431b1062d08972b0d28a74c5
Tallyon <tallyon.lima@sptech.school>
2023-11-15 23:29:32 -0200

    Modelagem completa

--- banco de dados/comandos sql do projeto.sql
+++ banco de dados/comandos sql do projeto.sql
@@ -14,7 +14,7 @@ senhaUsuario varchar(14)
 CREATE TABLE batalhas(
 idBatalha int primary key auto_increment,
 nomeBatalha varchar(70),
-siglas varchar(5),
+siglas varchar(7),
 apresentador1 varchar(45),
 apresentador2 varchar(45),
 emailBatalha varchar(80),
@@ -22,17 +22,37 @@ telefoneBatalha float(11),
 senhaBatalha varchar(14)
 )auto_increment=1000;
 
-CREATE TABLE relacao(
-fkUsuario int,
-fkBatalha int,
-idSeguir int,
-primary key (fkUsuario, fkBatalha, idSeguir),
-CONSTRAINT fkUsuarioSeguir FOREIGN KEY 	(fkUsuario) REFERENCES usuario(idUsuario),
-CONSTRAINT fkBatalhaSeguir FOREIGN KEY (fkBatalha) REFERENCES batalhas(idBatalha),
-numeroSeguidores int,
-curtidas int,
-CONSTRAINT chkCurtidas CHECK (curtidas IN (1)),
-CONSTRAINT chkNumeroSeguidores check (numeroSeguidores IN (1))
+CREATE TABLE seguidores(
+fkUsuarioS int,
+fkBatalhaS int,
+primary key (fkUsuarioS, fkBatalhaS),
+CONSTRAINT fkUsuarioSSeguir FOREIGN KEY (fkUsuarioS) REFERENCES usuario(idUsuario),
+CONSTRAINT fkBatalhaSSeguir FOREIGN KEY (fkBatalhaS) REFERENCES batalhas(idBatalha),
+seguindo int,
+CONSTRAINT chkSeguindoSeguidores check (seguindo IN (1))
+);
+
+CREATE TABLE rackBatalha(
+idRackBatalha int primary key,
+nomeMc varchar(45),
+qtdTitulo int,
+fkBatalhaRB int,
+CONSTRAINT idRackBatalhaRackBatakha FOREIGN KEY (fkBatalhaRB) REFERENCES batalhas(idBatalha)
+);
+
+
+CREATE TABLE post(
+idPost int primary key,
+conteudo varchar(500),
+dtPost DATETIME 
+);
+
+CREATE TABLE curtidas(
+fkPostC int,
+fkUsuarioC int,
+primary key(fkPostC, fkUsuarioC),
+curtida int,
+CONSTRAINT chkCurtidaCurtidas check (curtida IN (1))
 );
 
 INSERT INTO usuario VALUES
@@ -44,13 +64,12 @@ INSERT INTO batalhas VALUES
 (null, 'MC Alves','Mc Bob 13','Batalha da Aldeia', 'batalhadaaldeia@gmail.com', '11941873265','bda123'),
 (null, 'Mc Negralha', null, 'Batalha Do Coliseu', 'batalhadocoliseu@gmail.com', '11969369825','bdc123');
 
-INSERT INTO relacao VALUES
-(1,1000,1, '1','1'),
-(1,1000,2, '1','1'),
-(2,1001,3,'1','1'),
-(2,1000,4,'1','1');
 
 SELECT * FROM usuario JOIN relacao ON idUsuario=fkUsuario JOIN batalhas ON fkBatalha = idBatalha;
 
 SELECT u.nomeUsuario, b.nomeBatalha, r.numeroSeguidores FROM
  usuario AS u JOIN relacao AS r ON u.idUsuario = r.fkUsuario JOIN batalhas AS b ON r.fkBatalha = b.idBatalha;
+ 
+ SELECT * FROM usuario;
+ 
+ SELECT * FROM batalhas;
